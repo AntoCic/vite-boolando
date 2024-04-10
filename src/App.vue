7bc722a8a36de1588b90120ca3ade0401fe9b505
AntoCic <anto.cic.127@gmail.com>
2024-04-10 15:34:00 +0200

    CREATE base project

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,29 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>{{ titolo }}</h1>
+  <cmp-header />
+  <cmp-main />
+  <cmp-footer />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+import CmpHeader from './components/CmpHeader.vue'
+import CmpMain from './components/CmpMain.vue'
+import CmpFooter from './components/CmpFooter.vue'
+
+export default {
+  components: { CmpHeader, CmpMain, CmpFooter },
+  data() {
+    return {
+      titolo: "TITOLO"
+    }
+  }
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+</script>
+
+<style lang="scss">
+@use './styles/general';
+
+h1 {
+  text-align: center;
 }
 </style>
