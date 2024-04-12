96c9e3a4fd64fdfb42eda8520206f6f31a0b3dd3
AntoCic <anto.cic.127@gmail.com>
2024-04-12 15:40:18 +0200

    ADDED call with axios but not working correct

--- src/App.vue
+++ src/App.vue
@@ -13,15 +13,19 @@
 
 <script>
 import { store } from './store.js';
+import axios from 'axios';
 
 import CmpHeader from './components/CmpHeader.vue'
 import CmpMain from './components/CmpMain.vue'
 import CmpFooter from './components/CmpFooter.vue'
 
+
 export default {
   components: { CmpHeader, CmpMain, CmpFooter },
   mounted(){
-    // console.log(store);
+    axios.get('http://localhost:3000/products').then((res)=>{
+      store.products = res.dat
+    })
   }
 }
 </script>
